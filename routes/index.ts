d8e7d970edbb80be265e85cc2a2e599659bec582
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2022-03-13 00:58:31 +0330

    remove home

--- routes/index.ts
+++ routes/index.ts
@@ -5,7 +5,7 @@ import { logger } from '../services/logger-service'
 logger('info2', global.MONGODB_URI)
 const router = Router({strict: true})
 
-router.get('/', (req, res) => res.end('Home!!!'))
+// router.get('/', (req, res) => res.end('Home!!!'))
 router.get('/favicon.ico', (req, res) => res.end())
 router.use('/auth', AuthRoutes)
 router.use('/api/hi', Hi)
