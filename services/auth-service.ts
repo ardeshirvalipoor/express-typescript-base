d74688fdf8aa98faaa27da431305f38bc27fc35d
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2022-04-25 03:29:49 +0430

    added: auth service

--- services/auth-service.ts
+++ services/auth-service.ts
@@ -1,36 +1,84 @@
-// import * as passport from 'passport'
+import * as jwt from 'jsonwebtoken'
+import { Router, Request, Response, NextFunction } from 'express'
+import * as path from 'path'
+import MongoService from '../services/mongo-service'
+import cache from '../services/cache-service'
+import { emitter } from '../services/emitter-service'
+import { shortUUID } from '../utils/short-uuid'
+const { google } = require('googleapis')
 
-// export const requireAuth  = passport.authenticate('jwt', { session: false })
-// export const requireLogin = passport.authenticate('local', { session: false })
 
-// import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth'
-// import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL } from './configs'
 
-// export default (passport) => {
+export const getUrl = async (hostname: string, protocol: string, googleCredentials: any) => { // auth
+    const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL, GOOGLE_SCOPE } = googleCredentials
+    const prefix = protocol + '://' + hostname + (hostname == 'localhost' ? ':3000' : '')
+    const oauth2Client = new google.auth.OAuth2(
+        GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET,
+        prefix + GOOGLE_REDIRECT_URL
+    )
+    const url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: GOOGLE_SCOPE
+    })
+    return url
+}
 
-//     passport.serializeUser((user, done) => {
-//         done(null, user)
-//     })
+// export const getRedirectUrl = async (req, res) => { // Should be moved to the controller
+//     res.sendFile(path.join(__dirname + '/../public/app/auth.html'))
+// }
 
-//     passport.deserializeUser((user, done) => {
-//         done(null, user)
-//     })
+export const getCallback = (hostname: string, protocol: string, googleCredentials: any, code: string) => {
+    return new Promise(async (resolve, reject) => {
+        const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL, GOOGLE_SCOPE } = googleCredentials
+        const prefix = protocol + '://' + hostname + (hostname == 'localhost' ? ':3000' : '')
+        const oauth2Client = new google.auth.OAuth2(
+            GOOGLE_CLIENT_ID,
+            GOOGLE_CLIENT_SECRET,
+            prefix + GOOGLE_REDIRECT_URL
+        )
+        const { tokens } = await oauth2Client.getToken(code)
+        oauth2Client.setCredentials(tokens)
+        var oauth2 = google.oauth2({
+            auth: oauth2Client,
+            version: 'v2'
+        })
+        oauth2.userinfo.get(
+            (err: any, response: any) => {
+                if (err) {
+                    console.log(err)
+                    return reject(err)
+                } else {
+                    return resolve(response.data)
+                    // response.data
+                    // {
+                    //     id: string
+                    //     email: string
+                    //     verified_email: true,
+                    //     name: string
+                    //     given_name: strin
+                    //     family_name: strin
+                    //     picture: string
+                    //     locale: 'string
+                }
+            }
+        )
+    })
+}
 
-//     passport.use(new GoogleStrategy({
-//         clientID: GOOGLE_CLIENT_ID,
-//         clientSecret: GOOGLE_CLIENT_SECRET,
-//         callbackURL: GOOGLE_REDIRECT_URL
-//     },
-//         (accessToken, refreshToken, profile, cb) => {
-//             console.log(JSON.stringify({
-//                 accessToken,
-//                 refreshToken,
-//                 profile
-//             }));
-//             return cb(null, {
-//                 accessToken,
-//                 refreshToken,
-//                 profile
-//             })
-//         }))
+// export const roleAuthorization = (roles) => {
+//     return (req, res, next) => {
+//         var user = req.user
+//         /*         dummyUser.findById(user._id, (err, foundUser) => {
+//                     if (err) {
+//                         res.status(422).json({ error: 'No user found.' });
+//                         return next(err);
+//                     }
+//                     if (roles.indexOf(foundUser.role) > -1) {
+//                         return next();
+//                     }
+//                     res.status(401).json({ error: 'You are not authorized to view this content' });
+//                     return next('Unauthorized');
+//                 }); */
+//     }
 // }
