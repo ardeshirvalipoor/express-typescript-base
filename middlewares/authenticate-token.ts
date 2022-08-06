5bad05239915aad0d605a6d7ec5aaf3cbc09b879
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2022-08-06 16:36:16 +0430

    add authentication token

---
+++ middlewares/authenticate-token.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response, NextFunction } from 'express'
+import * as jwt from 'jsonwebtoken'
+
+export function authenticateToken(JWT_SECRET) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+        if (!token) {
+            res.status(401).json({
+                errors: [
+                    {
+                        msg: 'Token not found',
+                    },
+                ],
+            })
+        }
+
+        try {
+            const user: any = jwt.verify(token, JWT_SECRET)
+            req.user = { email: user.email }
+            next()
+        } catch (err) {
+            res.status(403).json({
+                errors: [
+                    {
+                        msg: 'Token is not valid',
+                    },
+                ],
+            })
+        }
+    }
+}
