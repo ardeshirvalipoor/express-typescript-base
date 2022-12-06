307b3d2c6b4990ecfb7dcb8cce94c5c36d6ddc88
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2022-12-06 16:12:30 +0100

    minor changes

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,7 +1,8 @@
 import authenticate from './authenticate'
 import authorize from './authorize'
+import errors from '../../middlewares.ts/errors'
 
 export default {
     authorize: authorize,
-    authenticate: authenticate
+    authenticate: authenticate,
 }
